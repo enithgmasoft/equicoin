983d059d0ed66662e31d977db4d6ee260d9a8b5d
Vettorazzi <vetto@hotmail.co.uk>
2018-04-02 10:52:02 -0500

    add nodes

--- src/net.cpp
+++ src/net.cpp
@@ -1173,6 +1173,7 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
+    {"default", "35.202.125.136"},
     {NULL, NULL}
 };
 
